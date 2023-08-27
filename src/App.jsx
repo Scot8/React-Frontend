ee832de9d20bea76827ecef9aa0379e4ca29ce87
Scot8 <rohansco18@gmail.com>
2023-08-26 21:02:03 -0400

    commit with changes updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,39 @@
 import { useState } from 'react';
 import React from 'react';
 
+let ctr = 0;
+
 function App() {
   const [todoForToday, setTodoForToday] = React.useState({
-    title: "go to gym",
+    title: "Go to gym",
     description: "Hit the gym from 7-9",
     id: 1
   });
 
-  setInterval(() => {
-    setTodoForToday({    
-      Title: "go to gym please please please",
-    description: "hit gym from 7-9",
-    id:1
-  })
+  // const [counter, setCounter] = React.useState(Math.random());
+  // var nonStateVarible = Math.random();
+  // console.log(todoForToday);
+  // console.log(counter);
+  // console.log(nonStateVarible);
 
-  }, 1000);
+  console.log("render");
+
+  if (ctr === 0) {
+    setInterval(() => {
+      setTodoForToday({    
+        Title: "go to gym please please please",
+      description: "hit gym from 7-9",
+      id:1
+    })
+  
+    }, 10000);
+
+    ctr = 1;
+  }
+  else{
+    
+  }
+
 
   return(
     <div>{todoForToday.title}
